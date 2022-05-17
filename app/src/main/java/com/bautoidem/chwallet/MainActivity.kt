34046a6d9a8429df32381235a591e85f821d1888
pvietlap <pvietlap@gmail.com>
2022-05-17 22:57:40 +0700

    - Initial app

---
+++ app/src/main/java/com/bautoidem/chwallet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bautoidem.chwallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
